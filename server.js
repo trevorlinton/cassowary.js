fdb911d0b930a63af3318334ab03b657bf40c071
Trevor Linton <trevor.linton@gmail.com>
2015-02-16 16:23:02 -0700

    still playing...

--- server.js
+++ server.js
@@ -1,68 +1,14 @@
-//
-// Simple static HTTP server that uses gzip compression (if client accepts gzip encoding)
-// by Richard Dancsi, www.wimagguc.com
-//
+var compression = require('compression');
+var express = require('express');
+var server  = express();
+var port    = Number(process.env.PORT || 5000);
 
-var DOCUMENT_ROOT = './';
-var DIRECTORY_INDEX = '/index.html';
+// Compress all possible responses
+server.use(compression());
 
-var port = process.env.PORT || 80;
+// Serve static content
+server.use(express.static(__dirname));
 
-var zlib = require('zlib');
-var http = require('http');
-var path = require('path');
-var fs = require('fs');
-
-http.createServer(function(request, response) {
-
-	// Remove query strings from uri
-	if (request.url.indexOf('?')>-1) {
-		request.url = request.url.substr(0, request.url.indexOf('?'));
-	}
-
-	// Remove query strings from uri
-	if (request.url == '/') {
-		request.url = DIRECTORY_INDEX;
-	}
-	var filePath = DOCUMENT_ROOT + request.url;
-
-	var extname = path.extname(filePath);
-
-	var acceptEncoding = request.headers['accept-encoding'];
-	if (!acceptEncoding) {
-		acceptEncoding = '';
-	}
-
-	path.exists(filePath, function(exists) {
-
-		if (exists) {
-			fs.readFile(filePath, function(error, content) {
-				if (error) {
-					response.writeHead(500);
-					response.end();
-				}
-				else {
-					var raw = fs.createReadStream(filePath);
-
-					if (acceptEncoding.match(/\bdeflate\b/)) {
-						response.writeHead(200, { 'content-encoding': 'deflate' });
-						raw.pipe(zlib.createDeflate()).pipe(response);
-					} else if (acceptEncoding.match(/\bgzip\b/)) {
-						response.writeHead(200, { 'content-encoding': 'gzip' });
-						raw.pipe(zlib.createGzip()).pipe(response);
-					} else {
-						response.writeHead(200, {});
-						raw.pipe(response);
-					}
-				}
-			});
-		}
-		else {
-			response.writeHead(404);
-			response.end();
-		}
-	});
-
-}).listen(port);
-
-console.log('Serving files on http://localhost:' + port);
+server.listen(port, function() {
+  console.log("Listening on port " + port);
+});
